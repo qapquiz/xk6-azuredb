18de0a31fdc86f0e8b745754bca2c9ff38b08991
armariya <ariya.lawanitchanon@scb.co.th>
2023-12-19 17:27:34 +0700

    feat: finish version 0.0.1

---
+++ azuredb.go
@@ -0,0 +1,64 @@
+package azuredb 
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/microsoft/go-mssqldb/azuread"
+	"go.k6.io/k6/js/modules"
+)
+
+// init is called by the Go runtime at application startup.
+func init() {
+	modules.Register("k6/x/azuredb", new(AzureDB))
+}
+
+type AzureDB struct {}
+
+func connectDatabase(connectionString string) (*sql.DB, error) {
+	db, err := sql.Open(
+	    azuread.DriverName,
+	    connectionString,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db, err
+}
+
+func (az *AzureDB) Insert(connectionString string, rawQueries []string) ([]int64, error) {
+	db, err := connectDatabase(connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	insertedIds := []int64 {}
+
+	for _, rawQuery := range rawQueries {
+		stmt, err := db.Prepare(rawQuery)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		result, err := stmt.Exec()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		lastId, err := result.LastInsertId()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		insertedIds = append(insertedIds, lastId)
+	}
+
+	return insertedIds, err
+}
